fc08226ae26b9f881baa3dcb41d59da8a89fd4d0
sebastian oscar lopez <sebastian.oscar.lopez@gmail.com>
2022-10-12 01:00:35 -0300

    poc: terrain tile deformation

--- src/render/draw_terrain.ts
+++ src/render/draw_terrain.ts
@@ -1,6 +1,10 @@
 import StencilMode from '../gl/stencil_mode';
 import DepthMode from '../gl/depth_mode';
-import {terrainUniformValues, terrainDepthUniformValues, terrainCoordsUniformValues} from './program/terrain_program';
+import {
+    terrainUniformValues,
+    terrainDepthUniformValues,
+    terrainCoordsUniformValues,
+} from './program/terrain_program';
 import type Painter from './painter';
 import type Tile from '../source/tile';
 import CullFaceMode from '../gl/cull_face_mode';
@@ -22,13 +26,36 @@ function drawDepth(painter: Painter, terrain: Terrain) {
     const tiles = terrain.sourceCache.getRenderableTiles();
     const program = painter.useProgram('terrainDepth');
     context.bindFramebuffer.set(terrain.getFramebuffer('depth').framebuffer);
-    context.viewport.set([0, 0, painter.width  / devicePixelRatio, painter.height / devicePixelRatio]);
+    context.viewport.set([
+        0,
+        0,
+        painter.width / devicePixelRatio,
+        painter.height / devicePixelRatio,
+    ]);
     context.clear({color: Color.transparent, depth: 1});
     for (const tile of tiles) {
         const terrainData = terrain.getTerrainData(tile.tileID);
-        const posMatrix = painter.transform.calculatePosMatrix(tile.tileID.toUnwrapped());
-        const uniformValues = terrainDepthUniformValues(posMatrix, terrain.getMeshFrameDelta(painter.transform.zoom));
-        program.draw(context, gl.TRIANGLES, depthMode, StencilMode.disabled, colorMode, CullFaceMode.backCCW, uniformValues, terrainData, 'terrain', mesh.vertexBuffer, mesh.indexBuffer, mesh.segments);
+        const posMatrix = painter.transform.calculatePosMatrix(
+            tile.tileID.toUnwrapped()
+        );
+        const uniformValues = terrainDepthUniformValues(
+            posMatrix,
+            terrain.getMeshFrameDelta(painter.transform.zoom)
+        );
+        program.draw(
+            context,
+            gl.TRIANGLES,
+            depthMode,
+            StencilMode.disabled,
+            colorMode,
+            CullFaceMode.backCCW,
+            uniformValues,
+            terrainData,
+            'terrain',
+            mesh.vertexBuffer,
+            mesh.indexBuffer,
+            mesh.segments
+        );
     }
     context.bindFramebuffer.set(null);
     context.viewport.set([0, 0, painter.width, painter.height]);
@@ -51,16 +78,40 @@ function drawCoords(painter: Painter, terrain: Terrain) {
     // draw tile-coords into framebuffer
     const program = painter.useProgram('terrainCoords');
     context.bindFramebuffer.set(terrain.getFramebuffer('coords').framebuffer);
-    context.viewport.set([0, 0, painter.width  / devicePixelRatio, painter.height / devicePixelRatio]);
+    context.viewport.set([
+        0,
+        0,
+        painter.width / devicePixelRatio,
+        painter.height / devicePixelRatio,
+    ]);
     context.clear({color: Color.transparent, depth: 1});
     terrain.coordsIndex = [];
     for (const tile of tiles) {
         const terrainData = terrain.getTerrainData(tile.tileID);
         context.activeTexture.set(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, coords.texture);
-        const posMatrix = painter.transform.calculatePosMatrix(tile.tileID.toUnwrapped());
-        const uniformValues = terrainCoordsUniformValues(posMatrix, 255 - terrain.coordsIndex.length, terrain.getMeshFrameDelta(painter.transform.zoom));
-        program.draw(context, gl.TRIANGLES, depthMode, StencilMode.disabled, colorMode, CullFaceMode.backCCW, uniformValues, terrainData, 'terrain', mesh.vertexBuffer, mesh.indexBuffer, mesh.segments);
+        const posMatrix = painter.transform.calculatePosMatrix(
+            tile.tileID.toUnwrapped()
+        );
+        const uniformValues = terrainCoordsUniformValues(
+            posMatrix,
+            255 - terrain.coordsIndex.length,
+            terrain.getMeshFrameDelta(painter.transform.zoom)
+        );
+        program.draw(
+            context,
+            gl.TRIANGLES,
+            depthMode,
+            StencilMode.disabled,
+            colorMode,
+            CullFaceMode.backCCW,
+            uniformValues,
+            terrainData,
+            'terrain',
+            mesh.vertexBuffer,
+            mesh.indexBuffer,
+            mesh.segments
+        );
         terrain.coordsIndex.push(tile.tileID.key);
     }
     context.bindFramebuffer.set(null);
@@ -71,27 +122,66 @@ function drawTerrain(painter: Painter, terrain: Terrain, tiles: Array<Tile>) {
     const context = painter.context;
     const gl = context.gl;
     const colorMode = painter.colorModeForRenderPass();
-    const depthMode = new DepthMode(gl.LEQUAL, DepthMode.ReadWrite, painter.depthRangeFor3D);
+    const depthMode = new DepthMode(
+        gl.LEQUAL,
+        DepthMode.ReadWrite,
+        painter.depthRangeFor3D
+    );
     const program = painter.useProgram('terrain');
     const mesh = terrain.getTerrainMesh();
 
     context.bindFramebuffer.set(null);
     context.viewport.set([0, 0, painter.width, painter.height]);
-
     for (const tile of tiles) {
         const texture = painter.renderToTexture.getTexture(tile);
         const terrainData = terrain.getTerrainData(tile.tileID);
+
+        //const tileCoords = lngLat2tile(tile.tileID.canonical.toLngLat(), tile.tileID.canonical.z);
+        const tileCoords = window.clickCoords ? lngLat2tile(window.clickCoords, tile.tileID.canonical.z) : {};
+        if (tile.tileID.canonical.x === tileCoords.x && tile.tileID.canonical.y === tileCoords.y) {
+            terrainData.u_terrain_exaggeration = 2;
+        }
         context.activeTexture.set(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, texture.texture);
-        const posMatrix = painter.transform.calculatePosMatrix(tile.tileID.toUnwrapped());
-        const uniformValues = terrainUniformValues(posMatrix, terrain.getMeshFrameDelta(painter.transform.zoom));
-        program.draw(context, gl.TRIANGLES, depthMode, StencilMode.disabled, colorMode, CullFaceMode.backCCW, uniformValues, terrainData, 'terrain', mesh.vertexBuffer, mesh.indexBuffer, mesh.segments);
+        const posMatrix = painter.transform.calculatePosMatrix(
+            tile.tileID.toUnwrapped()
+        );
+        const uniformValues = terrainUniformValues(
+            posMatrix,
+            terrain.getMeshFrameDelta(painter.transform.zoom)
+        );
+        program.draw(
+            context,
+            gl.TRIANGLES,
+            depthMode,
+            StencilMode.disabled,
+            colorMode,
+            CullFaceMode.backCCW,
+            uniformValues,
+            terrainData,
+            'terrain',
+            mesh.vertexBuffer,
+            mesh.indexBuffer,
+            mesh.segments
+        );
     }
-
 }
 
-export {
-    drawTerrain,
-    drawDepth,
-    drawCoords
-};
+function lngLat2tile({lng, lat}, zoom) {
+    const tileX = Math.floor(((lng + 180) / 360) * Math.pow(2, zoom));
+
+    const tileY = Math.floor(
+        ((1 -
+      Math.log(
+          Math.tan((lat * Math.PI) / 180) + 1 / Math.cos((lat * Math.PI) / 180)
+      ) /
+        Math.PI) /
+      2) *
+      Math.pow(2, zoom)
+    );
+
+    console.log({x: tileX, y: tileY});
+    return {x: tileX, y: tileY};
+}
+
+export {drawTerrain, drawDepth, drawCoords};
